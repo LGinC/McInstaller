468f3b91a49c625cbb656d64f729f1be5c5c3c23
liguo <liguo1976@outlook.com>
2021-03-11 11:35:09 +0800

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace McInstaller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.L_SysInfo = new System.Windows.Forms.Label();
+            this.CB_JreInstalled = new System.Windows.Forms.CheckBox();
+            this.CB_LauncherDownloaded = new System.Windows.Forms.CheckBox();
+            this.CB_JreDownloaded = new System.Windows.Forms.CheckBox();
+            this.CB_LauncherConfig = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(169, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "系统位数:";
+            // 
+            // L_SysInfo
+            // 
+            this.L_SysInfo.AutoSize = true;
+            this.L_SysInfo.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.L_SysInfo.Location = new System.Drawing.Point(260, 33);
+            this.L_SysInfo.Name = "L_SysInfo";
+            this.L_SysInfo.Size = new System.Drawing.Size(48, 19);
+            this.L_SysInfo.TabIndex = 0;
+            this.L_SysInfo.Text = "64位";
+            // 
+            // CB_JreInstalled
+            // 
+            this.CB_JreInstalled.AutoSize = true;
+            this.CB_JreInstalled.Enabled = false;
+            this.CB_JreInstalled.Location = new System.Drawing.Point(188, 114);
+            this.CB_JreInstalled.Name = "CB_JreInstalled";
+            this.CB_JreInstalled.Size = new System.Drawing.Size(90, 16);
+            this.CB_JreInstalled.TabIndex = 1;
+            this.CB_JreInstalled.Text = "JRE安装完成";
+            this.CB_JreInstalled.UseVisualStyleBackColor = true;
+            // 
+            // CB_LauncherDownloaded
+            // 
+            this.CB_LauncherDownloaded.AutoSize = true;
+            this.CB_LauncherDownloaded.Enabled = false;
+            this.CB_LauncherDownloaded.Location = new System.Drawing.Point(188, 149);
+            this.CB_LauncherDownloaded.Name = "CB_LauncherDownloaded";
+            this.CB_LauncherDownloaded.Size = new System.Drawing.Size(108, 16);
+            this.CB_LauncherDownloaded.TabIndex = 2;
+            this.CB_LauncherDownloaded.Text = "启动器下载完成";
+            this.CB_LauncherDownloaded.UseVisualStyleBackColor = true;
+            // 
+            // CB_JreDownloaded
+            // 
+            this.CB_JreDownloaded.AutoSize = true;
+            this.CB_JreDownloaded.Enabled = false;
+            this.CB_JreDownloaded.Location = new System.Drawing.Point(188, 82);
+            this.CB_JreDownloaded.Name = "CB_JreDownloaded";
+            this.CB_JreDownloaded.Size = new System.Drawing.Size(90, 16);
+            this.CB_JreDownloaded.TabIndex = 1;
+            this.CB_JreDownloaded.Text = "JRE下载完成";
+            this.CB_JreDownloaded.UseVisualStyleBackColor = true;
+            // 
+            // CB_LauncherConfig
+            // 
+            this.CB_LauncherConfig.AutoSize = true;
+            this.CB_LauncherConfig.Enabled = false;
+            this.CB_LauncherConfig.Location = new System.Drawing.Point(188, 185);
+            this.CB_LauncherConfig.Name = "CB_LauncherConfig";
+            this.CB_LauncherConfig.Size = new System.Drawing.Size(108, 16);
+            this.CB_LauncherConfig.TabIndex = 2;
+            this.CB_LauncherConfig.Text = "启动器安装完成";
+            this.CB_LauncherConfig.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(527, 310);
+            this.Controls.Add(this.CB_LauncherConfig);
+            this.Controls.Add(this.CB_LauncherDownloaded);
+            this.Controls.Add(this.CB_JreDownloaded);
+            this.Controls.Add(this.CB_JreInstalled);
+            this.Controls.Add(this.L_SysInfo);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "MineCraft Java版安装器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label L_SysInfo;
+        private System.Windows.Forms.CheckBox CB_JreInstalled;
+        private System.Windows.Forms.CheckBox CB_LauncherDownloaded;
+        private System.Windows.Forms.CheckBox CB_JreDownloaded;
+        private System.Windows.Forms.CheckBox CB_LauncherConfig;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Linq;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Compression;
+
+namespace McInstaller
+{
+    public partial class Form1 : Form
+    {
+        string baseUrl, jre32bitUrl, jre64bitUrl, mcLauncherUrl, jrePath, launcherPath;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            baseUrl = ConfigurationManager.AppSettings["BaseUrl"]?.Trim();
+            baseUrl = baseUrl?.EndsWith("/") ?? false ? baseUrl.Remove(baseUrl.Length - 2) : baseUrl;
+            jre32bitUrl = FormatUrl(ConfigurationManager.AppSettings["Jre32BitUrl"]?.Trim());
+            jre64bitUrl = FormatUrl(ConfigurationManager.AppSettings["Jre64BitUrl"]?.Trim());
+            mcLauncherUrl = FormatUrl(ConfigurationManager.AppSettings["McLauncherUrl"]?.Trim());
+            launcherPath = ConfigurationManager.AppSettings["McLauncherPath"]?.Trim();
+            jrePath = ConfigurationManager.AppSettings["JrePath"]?.Trim();
+            jrePath = String.IsNullOrEmpty(jrePath) ? Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "jre.exe") : jrePath;
+
+            CheckUrls();
+            Task.Run(CheckInstallAsync);
+        }
+
+        private async Task CheckInstallAsync()
+        {
+            BeginInvoke((Action)(async () =>
+            {
+                //安装jre
+                if (CheckJreExisted())
+                {
+                    CB_JreInstalled.Checked = true;
+                    CB_JreDownloaded.Checked = true;
+                }
+                else
+                {
+                    await DownloadJreAsync();
+                    InstallJreAsync();
+                }
+
+                //下载启动器
+                //1.检查下载路径配置
+                if (string.IsNullOrWhiteSpace(launcherPath))
+                {
+                    var dialog = new FolderBrowserDialog();
+                    if (dialog.ShowDialog() == DialogResult.OK)//TODO: 选择不为ok 则重复打开选择窗口
+                    {
+                        launcherPath = dialog.SelectedPath;
+                        UpdateConfiguration("McLauncherPath", launcherPath);
+                    }
+                }
+                //2.检查启动器是否存在
+                string fileName = mcLauncherUrl.Split('/').LastOrDefault();
+                var path = Path.Combine(launcherPath, fileName);
+                if (!File.Exists(path))
+                {
+                    //下载启动器
+                    await DownloadLauncherAsync(path);
+                }
+                CB_LauncherDownloaded.Checked = true;
+
+                //3.配置启动器
+                var ff = new FileInfo(fileName);
+                string folder = Path.Combine(launcherPath, ff.Name.Replace(ff.Extension, string.Empty));
+                if(!Directory.Exists(folder) || Directory.GetFiles(folder).Any(f=> f.Contains(".exe") || f.Contains(".EXE")))
+                {
+                    //如果文件夹不存在或者无exe文件则解压
+                    ZipFile.ExtractToDirectory(path, folder);
+                }
+
+                string exePath = Path.Combine(folder, Directory.GetFiles(folder).First(f => f.Contains(".exe") || f.Contains(".EXE")));
+                //桌面快捷方式
+                string shortcutPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), "MineCraft Launcher.lnk");
+                bool exist = false;
+                var shell = new IWshRuntimeLibrary.WshShell();
+                if (!File.Exists(shortcutPath))
+                {
+                    foreach (var item in Directory.GetFiles(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), "*.lnk"))
+                    {
+                        var ws = (IWshRuntimeLibrary.WshShortcut)shell.CreateShortcut(item);
+                        if (ws.TargetPath == exePath)
+                        {
+                            exist = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (!exist)
+                {
+                    var shortcut = (IWshRuntimeLibrary.WshShortcut)shell.CreateShortcut(shortcutPath);
+                    shortcut.TargetPath = exePath;
+                    shortcut.Arguments = string.Empty;
+                    shortcut.Description = "MC 启动器";
+                    shortcut.WorkingDirectory = folder;
+                    shortcut.IconLocation = exePath;
+                    shortcut.Save();
+                }
+
+                CB_LauncherConfig.Checked = true;
+                //启动启动器
+                var p = new Process();
+                p.StartInfo.FileName = exePath;
+                p.EnableRaisingEvents = false;
+                p.Start();
+                Application.Exit();
+            }));
+        }
+
+        private async Task DownloadLauncherAsync(string path)
+        {
+            if (await DownloadAsync(mcLauncherUrl, path) == false)
+            {
+                MessageBox.Show("启动器 下载失败，请检查网络后重试");
+                Application.Exit();
+            }
+
+        }
+
+        private void UpdateConfiguration(string key, string value)
+        {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            config.AppSettings.Settings[key].Value = value;
+            config.AppSettings.SectionInformation.ForceSave = true;
+            config.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection("appsettings");
+        }
+
+        private void InstallJreAsync()
+        {
+            using (var p = new Process())
+            {
+                p.StartInfo.FileName = jrePath;
+                p.EnableRaisingEvents = true;
+                bool succeed = false;
+                p.Exited += (s, e) =>
+                {
+                    succeed = (s as Process).ExitCode == 0;
+                };
+                p.Start();
+                p.WaitForExit();
+                //安装程序异常退出
+                if (!succeed)
+                {
+                    MessageBox.Show("jre 安装失败，请稍后重试");
+                    Application.Exit();
+                }
+            }
+
+            CB_JreInstalled.Checked = true;
+        }
+
+        /// <summary>
+        /// 下载JRE
+        /// </summary>
+        /// <returns></returns>
+        private async Task DownloadJreAsync()
+        {
+            if (File.Exists(jrePath))
+            {
+                CB_JreDownloaded.Checked = true;
+                return;
+            }
+
+            
+            if(await DownloadAsync(Environment.Is64BitOperatingSystem ? jre64bitUrl : jre32bitUrl, jrePath) == false)
+            {
+                MessageBox.Show("JRE 下载失败，请检查网络后重试");
+                Application.Exit();
+            }
+
+            CB_JreDownloaded.Checked = true;
+        }
+
+        private async Task<bool> DownloadAsync(string url, string path)
+        {
+            using(var client = new HttpClient())
+            {
+                int i = 0;
+                //下载失败重复3次
+                for (; i < 3; i++)
+                {
+                    try
+                    {
+                        var response = await client.GetAsync(url);
+                        if (!response.IsSuccessStatusCode)
+                        {
+                            continue;
+                        }
+
+                        var bytes = await response.Content.ReadAsByteArrayAsync();
+                        if (bytes != null && bytes.Length > 0)
+                        {
+                            File.WriteAllBytes(path, bytes);
+                            i = 0;//下载完成重置索引
+                            break;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        throw;
+                    }
+                }
+
+                return i == 0;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            L_SysInfo.Text = Environment.Is64BitOperatingSystem ? "64位" : "32位";
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+        }
+
+        private void CheckUrls()
+        {
+            string msg = string.Empty;
+            if (string.IsNullOrWhiteSpace(baseUrl))
+            {
+                msg = $"BaseUrl未设置 {Environment.NewLine}";
+            }
+
+            if (!Environment.Is64BitOperatingSystem && string.IsNullOrWhiteSpace(jre32bitUrl))
+            {
+                msg = $"{msg} Jre32BitUrl未设置 {Environment.NewLine}";
+            }
+
+            if(Environment.Is64BitOperatingSystem && string.IsNullOrWhiteSpace(jre64bitUrl))
+            {
+                msg = $"{msg} Jre64BitUrl未设置{Environment.NewLine}";
+            }
+
+            if (string.IsNullOrWhiteSpace(mcLauncherUrl))
+            {
+                msg = $"{msg} McLauncherUrl未设置{Environment.NewLine}";
+            }
+
+            if (!string.IsNullOrWhiteSpace(msg))
+            {
+                MessageBox.Show(msg, "配置不完全");
+                Application.Exit();
+            }
+        }
+
+        private string FormatUrl(string url)
+        {
+            if(string.IsNullOrWhiteSpace(baseUrl) || string.IsNullOrWhiteSpace(url) || url.Contains("http://") || url.Contains("https://"))
+            {
+                return url;
+            }
+
+            return $"{baseUrl}{(url.StartsWith("/")?url:$"/{url}")}";
+        }
+
+        private bool CheckJreExisted()
+        {
+            Process p = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "java.exe",
+                    Arguments = "-version",
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
+
+            try
+            {
+                p.Start();
+                var result = p.StandardError.ReadToEnd();
+                return result.Contains("java version");
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
